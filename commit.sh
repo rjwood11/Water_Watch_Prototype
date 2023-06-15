6759eb9dc08fe604b3cd55eebc8dd8ceaa7c19ff
rjwood11 <103776980+rjwood11@users.noreply.github.com>
2023-06-15 12:08:57 -0500

    Delete commit.sh

--- commit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd C:/Users/rwjac/Desktop/TNWaterWatch
-git add --all
-timestamp() {
-  date +"at %H:%M:%S on %d/%m/%Y"
-}
-git commit -am "Regular auto-commit $(timestamp)"
-git push origin master
