a107671de3497aa453c101bdea0d6efe7730a59e
rjwood11 <ryanjackwood@harpethriver.org>
2023-06-30 11:38:22 -0500

    Regular auto-commit at 11:38:22 on 30/06/2023

---
+++ render2.R
@@ -0,0 +1,9 @@
+#This script is what runs the R Markdown files in the Windows command prompt to allow for task schedule runs (i.e. automation)
+
+library(rmarkdown)
+Sys.setenv(RSTUDIO_PANDOC = 'C:/Program Files/RStudio/resources/app/bin/quarto/bin/tools')
+rmarkdown::render(
+  input = "C:/Users/rwjac/Desktop/Water_Watch_Prototype/index.Rmd",
+  output_file = "C:/Users/rwjac/Desktop/Water_Watch_Prototype/index.html",
+  output_dir = "C:/Users/rwjac/Desktop/Water_Watch_Prototype"
+)
