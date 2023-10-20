fedc601189a162fa5a8d3b7863cd4bfcd7b82df5
rjwood11 <103776980+rjwood11@users.noreply.github.com>
2023-10-20 12:49:01 -0500

    Delete commit.sh

--- commit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd C:/Users/Ryan/Desktop/Water_Watch_Prototype
-git add --all
-timestamp() {
-  date +"at %H:%M:%S on %d/%m/%Y"
-}
-git commit -am "Regular auto-commit $(timestamp)"
-git push origin
