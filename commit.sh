2e6afbafead57051741aa695b50a47ffea8139bc
rjwood11 <ryanjackwood@harpethriver.org>
2023-10-20 12:52:29 -0500

    Create commit.sh

---
+++ commit.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+cd C:/Users/rwjac/Desktop/Water_Watch_Prototype
+git add --all
+timestamp() {
+  date +"at %H:%M:%S on %d/%m/%Y"
+}
+git commit -am "Regular auto-commit $(timestamp)"
+git push origin
